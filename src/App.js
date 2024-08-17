f646e79c5e6af668bae5f5dcacfb120882650783
Anurag <anuragchandra1601@gmail.com>
2024-08-17 22:06:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!clicked ? (
+        <button onClick={handleClick}>Click here for the link!</button>
+      ) : (
+        <h1>Welcome!</h1>
+      )}
     </div>
   );
 }
